611cf0c060494e1c72141991a552b6a872df64a5
Tomasz Pawlikowski <tp4458@gmail.com>
2023-10-29 22:41:15 +0000

    time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,42 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+const timeDispEl = $("#currentDay")
+var sheduleWrap = $(".container-lg")
+var saveBtnEl = $(".saveBTN")   //cant be global so we know chich exact button is clicked
+let workDay = {
+    start: 6,
+    finish: 18
+}
+
+
+//how will the shedule entry be stored? array ? object time:entry?
+
+function timeNow(){
+  var currentTime = dayjs().format("DD-MM-YYYY HH:mm:ss");
+    timeDispEl.text(currentTime);
+  }
+timeNow()
+
+
+
+
+
+
+// ```md
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+// ```
